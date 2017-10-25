21a17b28eec32e37e5c662ead81c9040f9043680
KiernanCalla <32743330+KiernanCalla@users.noreply.github.com>
2017-10-26 00:25:41 +0100

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-print("Hello")
-print("Changed in another")
-print("World")
-print("More changes")
-<<<<<<< HEAD
-=======
-print("Still more changes")
->>>>>>> 4efb53be03f631c57fa2e749dbd4bb3ffb0f72fe
